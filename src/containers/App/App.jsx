440ee40962d1613eca367a72b5d79fa63352ed20
Firmino Changani <firmino.changani@gmail.com>
2021-10-09 18:55:07 +0100

    chore: setup orbit

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,5 +1,15 @@
+import { customTokens } from "../../common/styles/Theme";
+import Heading from "@kiwicom/orbit-components/lib/Heading";
+import ThemeProvider from "@kiwicom/orbit-components/lib/ThemeProvider";
+
 function App() {
-  return <main>Spacex UI</main>;
+  return (
+    <ThemeProvider theme={{ orbit: customTokens }}>
+      <main>
+        <Heading>Spacex UI</Heading>
+      </main>
+    </ThemeProvider>
+  );
 }
 
 export default App;
