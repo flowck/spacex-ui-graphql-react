59395b2554cbc861e2897bd03a7d107f945f9b00
Firmino Changani <firmino.changani@gmail.com>
2021-10-13 21:27:25 +0100

    feat(#2): add support for rows with links

--- src/common/components/Table/Table.tsx
+++ src/common/components/Table/Table.tsx
@@ -1,24 +1,45 @@
+import { Link } from "react-router-dom";
 import Tag from "@kiwicom/orbit-components/lib/Tag";
 import OrbitTable, { TableHead, TableBody, TableRow, TableCell } from "@kiwicom/orbit-components/lib/Table";
 
 type Column = { name: string; label?: string; isTag?: boolean };
-type Row = Record<string, string | number>;
+type Row = Record<string, string | number | boolean>;
 
 interface Props {
   rows: unknown[];
   columns: Column[];
+  rowLink?: string;
 }
 
-export function Table({ rows, columns }: Props) {
+export function Table({ rows, columns, rowLink }: Props) {
+  const renderTags = (value: boolean, name: string) => {
+    return (
+      <TableCell key={name}>
+        <Tag> {value ? "Succeeded" : "Failed"} </Tag>{" "}
+      </TableCell>
+    );
+  };
+
+  const renderLink = (row: Row, name: string) => {
+    return (
+      <TableCell key={name}>
+        <Link style={{ color: "initial", textDecoration: "none" }} to={`${rowLink}/${row.id}`}>
+          {row[name] || name}
+        </Link>
+      </TableCell>
+    );
+  };
+
   const renderColumns = (row?: Row) => {
     return columns.map(({ name, label, isTag }) => {
       // Render boolens as <Tag />
       if (row && isTag) {
-        return (
-          <TableCell key={name}>
-            <Tag> {row[name] ? "Succeeded" : "Failed"} </Tag>{" "}
-          </TableCell>
-        );
+        return renderTags(row[name] as boolean, name);
+      }
+
+      // Render rows with links
+      if (row && rowLink) {
+        return renderLink(row, name);
       }
 
       return <TableCell key={name}>{row ? row[name] : label || name}</TableCell>;
